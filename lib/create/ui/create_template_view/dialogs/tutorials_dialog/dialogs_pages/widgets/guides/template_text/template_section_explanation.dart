caee9c9cfff91e859df410bd62e66993196f4faf
igormidev <igor_midev@outlook.com>
2024-08-30 00:39:30 -0300

    macos tryfix

---
+++ lib/create/ui/create_template_view/dialogs/tutorials_dialog/dialogs_pages/widgets/guides/template_text/template_section_explanation.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TemplateSectionExplanation extends StatelessWidget {
+  const TemplateSectionExplanation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SelectableText(
+          'Divide your template into sections',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        const SelectableText(
+          'You can divide your text into sections to make it easier to understand and manage.\n'
+          'This is useful when you have a large template text, or when you want to separate the text into different parts.\n\n'
+          'Also, when using the created template, you will be able to copy only the text from the selected section if you want.\n\n'
+          'To add a new section, just click on the "+" button that appears when you hover over the template text.\n\n',
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/create/ui/create_template_view/dialogs/tutorials_dialog/dialogs_pages/widgets/guides/template_text/template_text_resume_guide.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TemplateTextResumeGuide extends StatelessWidget {
+  const TemplateTextResumeGuide({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SelectableText(
+          'Template text',
+          style: Theme.of(context).textTheme.displaySmall,
+        ),
+        const SelectableText(
+          'The template text is the main part of the template, '
+          'it is where you will write the text base text '
+          'that the whoevers is using the template see/copy see.\n'
+          'In that template text, you can use the variables '
+          'you created to make the text dynamic.\n',
+        ),
+      ],
+    );
+  }
+}
